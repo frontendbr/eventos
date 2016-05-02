427caa3eccf383bf984c6decff193608c8da041d
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-02 01:47:10 -0300

    Add some containers

---
+++ src/js/containers/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import MainHeader from '../components/main-header'
+import MainContent from '../components/main-content/index'
+import MainFooter from '../components/main-footer'
+
+const App = () => (
+  <div>
+    <MainHeader />
+    <MainContent />
+    <MainFooter />
+  </div>
+)
+
+export default App
