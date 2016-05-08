bb3adaa55e77829360005de2c447d0275e8840a1
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-07 22:09:25 -0300

    Update filter

--- src/js/containers/main-content/header-filter.js
+++ src/js/containers/main-content/header-filter.js
@@ -2,57 +2,35 @@
 
 import React from 'react'
 import FormSelect from '../../components/form-select'
-import SvgIcon from '../../components/svg-icon'
+import FormSearch from './form-search'
+
+const filterFields = [{
+  icon: { id: 'date', label: 'Data' },
+  label: 'Escolha um mês',
+  options: [
+    { text: 'Escolha um mês', value: '' },
+    { text: 'Janeiro', value: 'Janeiro' },
+    { text: 'Fevereiro', value: 'Fevereiro' },
+    { text: 'Março', value: 'Março' }
+  ]
+}, {
+  icon: { id: 'location', label: 'Local' },
+  label: 'Escolha um estado',
+  options: [
+    { text: 'Escolha um estado', value: '' },
+    { text: 'São Paulo', value: 'São Paulo' },
+    { text: 'Acre', value: 'Acre' },
+    { text: 'Paraná', value: 'Paraná' }
+  ]
+}]
 
 const HeaderFilter = () => (
   <nav className='filter'>
-    <FormSelect
-      key='select-date'
-      icon={{
-        id: 'date',
-        label: 'Data'
-      }}
-      label='Escolha um mês'
-      options={[{
-        text: 'Escolha um mês',
-        value: ''
-      }, {
-        text: 'Janeiro',
-        value: 'Janeiro'
-      }, {
-        text: 'Fevereiro',
-        value: 'Fevereiro'
-      }, {
-        text: 'Março',
-        value: 'Março'
-      }]}
-    />
+    {filterFields.map((props, index) => (
+      <FormSelect key={index} {...props} />
+    ))}
 
-    <FormSelect
-      key='select-location'
-      icon={{
-        id: 'location',
-        label: 'Local'
-      }}
-      label='Escolha um estado'
-      options={[{
-        text: 'São Paulo',
-        value: 'São Paulo'
-      }, {
-        text: 'Acre',
-        value: 'Acre'
-      }, {
-        text: 'Paraná',
-        value: 'Paraná'
-      }]}
-    />
-
-    <div className='search'>
-      <input className='form-control' type='search' placeholder='Busque por palavras chaves' />
-      <button className='search-btn' type='submit' role='button'>
-        <SvgIcon id='search' label='Search' />
-      </button>
-    </div>
+    <FormSearch />
   </nav>
 )
 
