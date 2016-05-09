85e7211d759725f0ef63e7dd9f6b7b1582f9c62e
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-09 01:03:23 -0300

    Add redux state on app component - resolves #49

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,20 @@
 
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './containers/app/index'
+import configureStore from './store/configure-store'
+import reducers from './reducers/index'
+
+const store = configureStore({ reducers })
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('[data-js="app"]')
 )
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
