17838d0578605e8e9b88c544dc13bf556712ebca
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-24 00:12:35 -0300

    Update aside

--- src/js/containers/main-content/aside.js
+++ src/js/containers/main-content/aside.js
@@ -5,8 +5,16 @@ import React from 'react'
 const Aside = () => (
   <aside className='disclaimer'>
     <div className='disclaimer-inner'>
-      <h2 className='title'>Conhece algum evento de Front-End que não está listado?</h2>
-      <a href='https://github.com/frontendbr/eventos/issues' className='btn' aria-label='Anunciar Evento' title='Anuncie seu evento'>Anunciar Evento</a>
+      <h2 className='title'>
+        Conhece algum evento de Front-End que não está listado?
+      </h2>
+      <a
+        href='https://github.com/frontendbr/eventos/issues'
+        className='btn'
+        aria-label='Anunciar Evento'
+        title='Anuncie seu evento'>
+        Anunciar Evento
+      </a>
     </div>
   </aside>
 
