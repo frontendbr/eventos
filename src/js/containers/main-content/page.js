f8f9de45385cf0d986d3f398be69e95633b22805
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-24 00:12:54 -0300

    Update events on page

--- src/js/containers/main-content/page.js
+++ src/js/containers/main-content/page.js
@@ -1,26 +1,11 @@
 'use strict'
 
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 import EventListItem from '../../components/event-list-item'
-import Pagination from '../../components/pagination/index'
+// import Pagination from '../../components/pagination/index'
 
-const events = [{
-  title: 'Conferência CSS Brasil',
-  image: 'http://www.conferenciacssbrasil.com.br/build/img/css-brasil.png',
-  innerLink: 'inner.html',
-  link: 'http://www.conferenciacssbrasil.com.br/',
-  date: {
-    day: 20,
-    month: 'Maio',
-    year: 2016
-  },
-  price: 'R$ 120,00',
-  location: 'Maksound Plaza, São Paulo - SP',
-  locationUrl: 'https://www.google.com.br/maps?q=maksoud+plaza&um=1&ie=UTF-8&sa=X&ved=0ahUKEwinuq_pm4rMAhXKkZAKHfMfBgoQ_AUICCgC',
-  shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti vero assumenda aliquid voluptatibus quos vitae ratione aut maiores quo odio.'
-}]
-
-const Page = () => (
+const Page = ({ events }) => (
   <section className='page'>
     <header className='page-header'>
       <h3 className='title'>Próximos Eventos</h3>
@@ -30,10 +15,18 @@ const Page = () => (
       {events.map((props, index) => (
         <EventListItem key={index} {...props} />
       ))}
-      <Pagination />
+      {/* <Pagination /> */}
     </div>
 
   </section>
 )
 
-export default Page
+Page.propTypes = {
+  events: PropTypes.shape.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  events: state.events.events.filter((_, index) => index >= 5)
+})
+
+export default connect(mapStateToProps)(Page)
