8b6bc3f8f81b4c10774f34ba2c71f04b3f7b0edd
Fernando Daciuk <f.daciuk@gmail.com>
2016-06-01 09:41:48 -0300

    Show filter

--- src/js/containers/main-content/header.js
+++ src/js/containers/main-content/header.js
@@ -3,7 +3,7 @@
 import React, { PropTypes, Component } from 'react'
 import { connect } from 'react-redux'
 import HeaderBanner from './header-banner'
-// import HeaderFilter from './header-filter'
+import HeaderFilter from './header-filter'
 import { fetchEvents } from '../../actions/index'
 
 class Header extends Component {
@@ -12,25 +12,40 @@ class Header extends Component {
   }
 
   render () {
+    const { events, monthFilter, stateFilter, searchField } = this.props
     return (
       <header className='wellness'>
         <h1 className='title'>
           Encontre eventos de Front-End em todo o Brasil
         </h1>
 
-        <HeaderBanner events={this.props.events} />
-        {/* <HeaderFilter /> */}
+        <HeaderBanner events={events} />
+        <HeaderFilter
+          monthFilter={monthFilter}
+          stateFilter={stateFilter}
+          searchField={searchField}
+        />
       </header>
     )
   }
 }
 
+Header.defaultProps = {
+  searchField: ''
+}
+
 Header.propTypes = {
-  events: PropTypes.shape.isRequired
+  events: PropTypes.array.isRequired,
+  monthFilter: PropTypes.object.isRequired,
+  stateFilter: PropTypes.object.isRequired,
+  searchField: PropTypes.string.isRequired
 }
 
 const mapStateToProps = (state) => ({
-  events: state.events.events.filter((_, index) => index < 5)
+  events: state.events.events.filter((_, index) => index < 5),
+  monthFilter: state.filter.months,
+  stateFilter: state.filter.state,
+  searchField: state.filter.searchField
 })
 
 export default connect(mapStateToProps)(Header)
