3f7f521d63ff1e312cdb55e868b8a4ca8d6c0a9d
Fernando Daciuk <f.daciuk@gmail.com>
2016-06-07 09:32:45 -0300

    Update filter reducer

--- src/js/reducers/filter.js
+++ src/js/reducers/filter.js
@@ -1,32 +1,44 @@
 'use strict'
 
+import {
+  EVENTS_SUCCESS,
+  FILL_FILTER_SELECTS
+} from '../actions/index'
+
 const initialState = {
   months: {
     icon: { id: 'date', label: 'Data' },
     label: 'Escolha um mês',
-    options: [
-      { text: 'Escolha um mês', value: '' },
-      { text: 'Janeiro', value: 'Janeiro' },
-      { text: 'Fevereiro', value: 'Fevereiro' },
-      { text: 'Março', value: 'Março' }
-    ]
+    options: [{
+      text: 'Escolha um mês', value: ''
+    }]
   },
 
   state: {
     icon: { id: 'location', label: 'Estado' },
     label: 'Escolha um estado',
-    options: [
-      { text: 'Escolha um estado', value: '' },
-      { text: 'São Paulo', value: 'São Paulo' },
-      { text: 'Acre', value: 'Acre' },
-      { text: 'Paraná', value: 'Paraná' }
-    ]
+    options: [{
+      text: 'Escolha um estado', value: ''
+    }]
   },
 
   searchField: ''
 }
 
 const filter = (state = initialState, action) => {
+  switch (action.type) {
+    case FILL_FILTER_SELECTS:
+      return {
+        ...state,
+        [action.payload.select]: {
+          ...state[action.payload.select],
+          options: [
+            ...state[action.payload.select].options,
+            ...action.payload.options
+          ]
+        }
+      }
+  }
   return state
 }
 
