4f815bc544d96e35d84b82f3afab54a62a5ed547
Fernando Daciuk <f.daciuk@gmail.com>
2016-08-01 23:51:20 -0300

    Update components

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,33 @@
 
 import React from 'react'
 import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
-import App from './containers/app/index'
 import configureStore from './store/configure-store'
 import reducers from './reducers/index'
+import App from './containers/app/index'
 
 const store = configureStore({ reducers })
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('[data-js="app"]')
-)
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <Provider store={store}>
+        <NextApp />
+      </Provider>
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./containers/app/index', () => {
+    const NextApp = require('./containers/app/index').default
+    renderApp(NextApp)
+  })
+}
 
 store.subscribe(() => {
   console.log(store.getState())
