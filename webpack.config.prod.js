6a0e922b253c93438c94f12a0f3692d07ef583ff
Fernando Daciuk <f.daciuk@gmail.com>
2016-08-01 23:57:46 -0300

    Update build command and create webpack.config.prod

---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const validator = require('webpack-validator');
+
+module.exports = validator({
+  entry: path.join(__dirname, 'src', 'js', 'index.js'),
+
+  output: {
+    path: path.join(__dirname, 'build', 'js'),
+    filename: 'main.min.js'
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules|src\/server/,
+      include: /src/
+    }]
+  }
+});
